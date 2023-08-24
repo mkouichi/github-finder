b949902fb6127850c160895295ddee7f9ceec89c
mkouichi <kic.mhr.qs@gmail.com>
2023-08-24 17:26:20 -0400

    App setup with Tailwind and DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-purple-500'>
+      <h1 className='text-xl'>Tailwind check</h1>
+      <button className='btn bg-indigo-500 ...'>daisyUI check</button>
     </div>
   );
 }
