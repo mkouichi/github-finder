52557d1f4a20e374bd1ad7bbdd0dac4761524cfe
mkouichi <kic.mhr.qs@gmail.com>
2023-08-28 13:47:45 -0400

    Move alert component to home page (remove it from unneccesary pages)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import Alert from './components/layout/Alert';
 
 function App(): JSX.Element {
   return (
@@ -13,7 +12,6 @@ function App(): JSX.Element {
         <div className='flex flex-col h-screen'>
           <Navbar />
           <main className='container mx-auto px-3 pb-12 flex-grow'>
-            <Alert />
             <Outlet />
           </main>
           <Footer />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Alert from '../components/layout/Alert';
 import UserResults from '../components/users/UserResults';
 import UserSearch from '../components/users/UserSearch';
 
 function Home(): JSX.Element {
   return (
     <>
+      <Alert />
       <UserSearch />
       <UserResults />
     </>
